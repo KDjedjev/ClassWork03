9cbe37fc27895b754d57971d4cf428df60f447cc
kokodzh <kdjedjev@gmail.com>
2016-05-12 21:51:43 +0300

    ClassWork0.1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Scanner input = new Scanner(System.in);
+		System.out.print("Chisloto e: ");
+		int a = input.nextInt();
+		if(a<0 && a%2==0){
+			System.out.print("Negative even ");
+		}else if(a<0 && a%2==1){
+			System.out.print("Negative odd");
+		}else if(a>0 && a%2==0){
+			System.out.print("Positive Even");
+		}else if(a>0 && a%2==1){
+			System.out.print("Positive Odd");
+		}else{
+			System.out.print("Nula");
+		}
+		input.close();		
 	}
 
 }
